20d66afbf58d13ce8c48efa32b78b4c78b910578
nino <ninopaunov93@outlook.com>
2022-05-28 18:08:03 +0200

    almost done

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let myObject = {}
 
 const customerName = document.querySelector('#customerName')
 const dinoName = document.querySelector('#dino-select')
-const napomena = document.querySelector('#napomena')
+const Napomena = document.querySelector('#napomena')
 const form = document.querySelector('form')
 const ispisKupaca = document.querySelector('#ispisKupaca')
 
@@ -11,22 +11,23 @@ const ispisKupaca = document.querySelector('#ispisKupaca')
 form.addEventListener('submit', e => {
     e.preventDefault()
 
+
     if (customerName.value !== '' && dinoName.value !== '') {
 
         if (customerName.value.charAt(0) === customerName.value.charAt(0).toUpperCase()) {
 
             if (customerName.value.length > 3) {
-                
+
+
                 myObject = {
                     kupac: customerName.value.trim(),
                     dinosaurus: dinoName.value,
-                    napomena: napomena.value
+                    napomena: Napomena.value.trim() !== '' ? Napomena.value.trim() : 'poruka' 
                 }
-              
-                
                 myArray.push(myObject)
                 console.log(myArray)
-              
+
+
             } else {
                 alert('ime mora imati vise od 4 slova')
                 return
@@ -40,41 +41,49 @@ form.addEventListener('submit', e => {
         alert('morate popuniti polja')
         return
     }
+
 })
 
 const isipisiNarudzbine = () => {
-    let ispisJednogKupca = document.createElement('div')
-    ispisJednogKupca.classList.add('jedanKupac')
+    const prikazanKupci = document.querySelectorAll('.jedanKupac')
+    const kupciPrikazani = prikazanKupci.length > 0
 
-    myArray.forEach((e, index) => {
-        let kupac = document.createElement('p')
-        kupac = e.kupac
+    if (kupciPrikazani) {
+        return
+    }
+        myArray.forEach((e, index) => {
+            let ispisJednogKupca = document.createElement('div')
+            ispisJednogKupca.classList.add('jedanKupac')
 
-        let opis = document.createElement('p')
-        opis = e.napomena
+            let kupac = document.createElement('p')
+            kupac = e.kupac
 
-        let dino = document.createElement('p')
-        dino = e.dinosaurus
+            let opis = document.createElement('p')
+            opis = e.napomena
 
-        let dugme = document.createElement('BUTTON')
-        dugme.innerHTML = `obrisi`
-        dugme.onclick = izbrisi(index)
-        // dugme.setAttribute('id', 'izbrisiDugme')
-        
 
-        ispisJednogKupca.innerHTML = `
+            let dino = document.createElement('p')
+            dino = e.dinosaurus
+
+            let dugme = document.createElement('BUTTON')
+            dugme.innerHTML = `obrisi`
+            dugme.onclick = (index) => {
+
+                if (myArray.length > 0) {
+
+                    myArray.splice(index, 1)
+                    ispisJednogKupca.remove()
+                }
+                console.log(myArray)
+            }
+
+            ispisJednogKupca.innerHTML = `
                 <p><span>Kupac:</span> ${kupac}</p> <br>
                 <p><span>Napomena:</span> ${opis}</p> <br> 
                 <p><span>Dinosaurus:</span> ${dino}</p> <br>
-                
-        `
+                `
+            ispisJednogKupca.append(dugme)
+            ispisKupaca.append(ispisJednogKupca)
 
-        ispisJednogKupca.append(dugme)
-    })
-    
-    ispisKupaca.append(ispisJednogKupca)
-}
-const izbrisi = (index) => {
-    console.log(index)
-    myArray.splice(index, 1)
-}
+        })
+    }
