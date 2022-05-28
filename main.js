e3f802ed3381646f6de7967e16b959856a8fe029
nino <ninopaunov93@outlook.com>
2022-05-28 13:42:46 +0200

    first validation

--- main.js
+++ main.js
@@ -6,29 +6,35 @@ const dinoName = document.querySelector('#dino-select')
 const napomena = document.querySelector('#napomena')
 const form = document.querySelector('form')
 
+
 form.addEventListener('submit', e => {
     e.preventDefault()
 
-    if(customerName.value !=='' && dinoName.value !==''){
+    if (customerName.value !== '' && dinoName.value !== '') {
 
-        if(customerName.value.length > 3){
-            
-        myObject = {
-            kupac: customerName.value.trim(),
-            dinosaurus: dinoName.value,
-            napomena: napomena.value 
-        }
-    
-        myArray.push(myObject)
-        console.log(myArray)
+        if (customerName.value.charAt(0) === customerName.value.charAt(0).toUpperCase()) {
+
+            if (customerName.value.length > 3) {
+
+                myObject = {
+                    kupac: customerName.value.trim(),
+                    dinosaurus: dinoName.value,
+                    napomena: napomena.value
+                }
+
+                myArray.push(myObject)
+                console.log(myArray)
+            } else {
+                alert('ime mora imati vise od 4 slova')
+                return
+            }
         } else {
-            alert('ime mora imati vise od 4 slova')
-        } 
+            alert('Prvo slovo mora biti veliko')
+            return
+        }
 
     } else {
         alert('morate popuniti polja')
         return
     }
-
-
 })
